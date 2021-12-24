a29fb36c25b68226ad55c7915c2c96dade145a16
rjshearme <rjshearme@gmail.com>
2021-12-24 02:49:36 +0000

    changing redirect URI

--- constants.py
+++ constants.py
@@ -5,7 +5,7 @@ import string
 CLIENT_SECRET = os.getenv("CLIENT_SECRET")
 CLIENT_ID = os.getenv("CLIENT_ID")
 SCOPE = "user-library-read playlist-modify-public playlist-modify-private ugc-image-upload"
-REDIRECT_URI = "http://127.0.0.1:8080/callback"
+REDIRECT_URI = "http://127.0.0.1:8080/callback" if os.getenv("LOCAL_DEV") else "https://spotify-recently-liked.herokuapp.com/callback"
 AUTH_URL = "https://accounts.spotify.com/api/token"
 STATE = "".join(random.choice(string.ascii_lowercase) for _ in range(16))
 API_URL = "https://api.spotify.com/v1"
